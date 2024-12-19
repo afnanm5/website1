dc9cbfccc43d9547489544f03b2694e588a273f5
Afnan Monzur <afnanm.0505@gmail.com>
2024-12-19 00:28:40 -0500

    Fix root layout tags and update layout structure

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,24 +1,24 @@
 'use client'
 
 import '../styles/globals.css';
-import { ReactNode } from 'react';
 
-interface LayoutProps {
-  children: ReactNode;
-}
+export default function Layout({ children }) {
+    return (
+        <html lang="en">
+            <head>
+                {/* You can add custom head tags like meta, title, etc. */}
+            </head>
+            <body className="bg-black text-white">
+                {/* Background Video */}
+                <video autoPlay loop muted className="absolute top-0 left-0 w-full h-full object-cover z-[-1]">
+                    <source src="https://motionbgs.com/media/5044/abstract-blue-smoke.960x540.mp4" type="video/mp4" />
+                </video>
 
-export default function Layout({ children }: LayoutProps) {
-  return (
-    <div className="relative min-h-screen bg-black text-white">
-      {/* Background Video */}
-      <video autoPlay loop muted className="absolute top-0 left-0 w-full h-full object-cover z-[-1]">
-        <source src="https://motionbgs.com/media/5044/abstract-blue-smoke.960x540.mp4" type="video/mp4" />
-      </video>
-
-      {/* Content Area */}
-      <div className="relative z-10 p-8">
-        {children}
-      </div>
-    </div>
-  );
+                {/* Content Area */}
+                <div className="relative z-10 p-8">
+                    {children}
+                </div>
+            </body>
+        </html>
+    );
 }
